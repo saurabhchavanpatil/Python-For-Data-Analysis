0a297b829532e26c669eb88f30a72624f1bf7d6f
Saurabh Chavan <84279350+saurabhchavanpatil@users.noreply.github.com>
2024-06-25 14:07:57 +0530

    Add files via upload

--- Python for Data Analysis/Python Adv/Inheritance 2.py
+++ Python for Data Analysis/Python Adv/Inheritance 2.py
@@ -1,6 +1,6 @@
 #MUlTIPLE INHERITAN
 #  the one or more parent class can be
-# accesible for child class,the accesible methode is nothing but inheritance
+# accesible for child class,the accesible methode is nothing but ineritance
 # Given Ex the Student And Marks is Parent class of Result class,Result clas
 # is a child class of Student and Marks.
 class Student:
